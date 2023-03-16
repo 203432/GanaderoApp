da881f989c7e27f2c26890a4d006ada5cd3f8005
JorgeCamToledo <camacho.ja0402@gmail.com>
2023-03-16 15:03:16 -0600

    local variables

--- AnimalsBack/settings.py
+++ AnimalsBack/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iqy@dof&wx!e@lno9l%ds*_jvrg48t_$t-6#k5-8_#8v28=1p6'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,12 +91,12 @@ WSGI_APPLICATION = 'AnimalsBack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'AnimalsRegister', 
-        'USER': 'postgres', 
-        'PASSWORD': 'Nintendo64',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': os.getenv("ENGINE"),
+        'NAME': os.getenv("NAME"),
+        'USER':os.getenv("USER"),
+        'PASSWORD':os.getenv("PASSWORD"),
+        'HOST':os.getenv("HOST"),
+        'PORT':os.getenv("PORT")
     }
 }
 
